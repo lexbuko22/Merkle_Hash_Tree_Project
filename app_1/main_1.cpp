68c0d428587a5a520b89dd2f85ddaa2918cb2aab
lexbuko22 <lexbuko22@yahoo.com>
2023-05-07 14:40:42 -0600

    Developed app file for demonstration

--- app_1/main_1.cpp
+++ app_1/main_1.cpp
@@ -1,47 +1,26 @@
 #include <iostream>
 #include <fstream>
-
+#include "/Users/lexbukowski/Documents/CSPB-2270/term-project-lexbuko22/code_1/HashTree.h"
 using namespace std;
 
-
-/*
-unsigned int DJB2(std::string key){
-  unsigned int hash = 5381;
-  // Leaving the debugging stuff commented out, in case you want to
-  // play. It will output the hash at each incremental step so you can
-  // see how it takes shape.
-  //
-  //  cout << "Hashing string \"" << key << "\"" << endl;
-  //  cout << bitset<32>(hash) << " " << hash << endl;
-  for (size_t i=0; i < key.length(); i++) {
-    char c = key[i]; 
-    hash = ((hash << 5) + hash) + c;
-    // cout << bitset<32>(hash) << " " << hash << endl;
-  }
-  // cout << "Hashed string \"" << key << "\" to " << hash << endl;
-  //
-  // for light reading on why djb2 might not be the best
-  // production-grade hash function, go to
-  // http://dmytry.blogspot.com/2009/11/horrible-hashes.html
-  return hash;
-}
-*/
 int main()
 {
 
-//cout << DJB2(to_string(1899416666)+to_string(3342531326));
-return 0;
+HashTree myHashTree;
+vector<string> data_1 = {"car","bike","train"};
+vector<string> data_2 = {"hockey","formula 1","volleyball"};
+cout << myHashTree.Add_Data_To_Tree("truck",data_1) << endl;
+
+vector<string> data_3 = {"car","bike","train","truck"};
+cout << myHashTree.Build_Hash_Tree(data_3) << endl;
+
+if (myHashTree.Identical_Data(data_1,data_2))
+{
+    cout<< "Data sets are identical";
 }
 
-// apples hash = 4065158538
-// bananas hash = 1707924825
-// chocolate hash = 1216302135
-// ice cream hash = 3342531326
-// hockey hash = 42484808
+else cout << "Data sets are not identical";
 
-//apples + bananas hash = 2271817439
-// chocolate + hockey = 2618300579
 
-//applesbananas + chocolatehockey = 1899416666
-
-//applesbananaschocolatehockey + ice cream = 351600701
+return 0;
+}
